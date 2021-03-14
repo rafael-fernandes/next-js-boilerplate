0a24811071ab752b4e88b9744aa929f2600dfa8d
Rafael Alves Fernandes <spnrafael@gmail.com>
2021-03-14 17:39:39 -0300

    fix: test description typo

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import Main from '.'
 
 describe('<Main />', () => {
-  it('should hender the heading', () => {
+  it('should render the heading', () => {
     const { container } = render(<Main />)
 
     expect(
